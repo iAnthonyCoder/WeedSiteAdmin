6924896c9d604dbdb6edb1e5104418f08b6ae62c
anthony952 <anthonymedina958@hotmail.com>
2020-05-23 10:26:43 -0400

    fixed api url

--- src/_helpers/config.js
+++ src/_helpers/config.js
@@ -1,4 +1,4 @@
-const getApi = process.env.REACT_APP_API_URL;
-// const getApi = "http://localhost:3000/api/"
+// const getApi = process.env.REACT_APP_API_URL;
+const getApi = "https://project-tim.herokuapp.com/api/"
 
 export { getApi };
