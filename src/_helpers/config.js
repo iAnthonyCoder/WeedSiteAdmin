391da76d52feede56e2c2858082c2c9aa123258b
anthony952 <anthonymedina958@hotmail.com>
2020-05-29 05:32:05 -0400

    first rev #Meet1

--- src/_helpers/config.js
+++ src/_helpers/config.js
@@ -1,5 +1,8 @@
-const getPrivateApi = "http://localhost:3000/api/v1/private/";
-const getPublicApi = "http://localhost:3000/api/v1/public/";
+// const getPrivateApi = "http://localhost:3000/api/v1/private/";
+// const getPublicApi = "http://localhost:3000/api/v1/public/";
+
+const getPrivateApi = "https://project-tim.herokuapp.com/api/v1/private/";
+const getPublicApi = "https://project-tim.herokuapp.com/api/v1/public/";
 // const getApi = "https://project-tim.herokuapp.com/api/v1/private/"
 
 export { getPrivateApi, getPublicApi };
