9fcd74537cdca01b5eea6ebbe2fd6e9e52befa81
anthony952 <anthonymedina958@hotmail.com>
2020-06-07 00:07:02 -0400

    updated overview

--- src/admin/overview.jsx
+++ src/admin/overview.jsx
@@ -13,9 +13,12 @@ function Overview({ match }) {
             <strong>EDMUNDO:</strong> PRODUCTOS POR BRAND DESDE LA Z DESCENDENTE
             </h2>
             <p><Link to={`${path}/products/create`}><button type="button" class="btn btn-success">AGREGAR PRODUCTOS</button></Link></p>
-<hr></hr>
+            <hr></hr>
             <h2 style={{color:"red"}}>
             <strong>FELIX:</strong> AGREGAR STRAINS DESDE <a target="_blank" href="https://www.leafly.com/strains?sort=name" style={{color:"blue"}}>AQUI</a><br></br>
+            </h2>
+            <h2 style={{color:"red"}}>
+            <strong>FELIX:</strong> LA DESCRIPTION SE TOMARA DESDE <a target="_blank" href="https://www.wikileaf.com" style={{color:"blue"}}>WWW.WIKILEAF.COM</a> Y SE COLOCARA EN LA REFERENCIA "WWWW.WIKILEAF.COM"<br></br>
             </h2>
             <p><Link to={`${path}/strains/create`}><button type="button" class="btn btn-success">AGREGAR STRAINS</button></Link></p>
             <br></br><br></br>
